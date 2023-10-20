3e7255fe3e312f0eeb960548cb29957d3a72fede
ViTaUM <vitaum.alves@gmail.com>
2023-10-20 13:35:30 -0300

    app jogo da memoria

---
+++ src/constants/cartas.js
@@ -0,0 +1,23 @@
+const imagens = [
+  "eva.png",
+  "pipi.png",
+  "julia.png",
+  "gigi.png",
+  "mari.png",
+  "flora.png",
+  "nicolas.png",
+  "davi.png",
+  "catarina.png",
+  "hugo.png",
+  "giovana.png",
+  "marina.png",
+  "lara.png",
+  "flor-de-lis.png",
+  "gustavo.png",
+];
+
+const cartasUnicas = imagens.map((imagem, idDoPar) => ({ imagem, idDoPar }));
+
+export const paresDeCartas = [...cartasUnicas, ...cartasUnicas].map(
+  (props, id) => ({ ...props, id })
+);
---
+++ src/constants/resultados.js
@@ -0,0 +1,17 @@
+export const resultados = [
+  {
+    titulo: "Excelente",
+    imagem: "kitekat-4.png",
+    min: 75,
+  },
+  {
+    titulo: "Parabéns",
+    imagem: "kitekat-3.png",
+    min: 50,
+  },
+  {
+    titulo: "Bom",
+    imagem: "kitekat-3.png",
+    min: 25,
+  },
+];
